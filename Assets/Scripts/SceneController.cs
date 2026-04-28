4dd0caf236228f0422592244dab0c6584f4208a6
S Ohana <sophie.oh1@icloud.com>
2026-04-28 15:42:15 +0100

    SceneController
    added empty gameobject scenecontroller in each scene, created scene controller script.

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
